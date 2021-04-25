22a8a68893f1ab2ede8bfe50ed3af2d61de47069
betty214 <50865204+betty214@users.noreply.github.com>
2021-04-25 14:26:58 +0900

    http->https userservice 라우팅 변경

--- src/service/UserService.js
+++ src/service/UserService.js
@@ -1,43 +1,43 @@
 import axios from 'axios';
 class UserService {
     createUser(user) {
-        return axios.post("http://www.graduate11.com:8080/api/user", user);
+        return axios.post("https://www.graduate11.com:8080/api/user", user);
     }
     loginUser(user) {
         return axios.post("https://www.graduate11.com:8080/api/login", user);
     }
     TimeUser(time) {
-        return axios.post("http://www.graduate11.com:8080/api/tableCheck", time);
+        return axios.post("https://www.graduate11.com:8080/api/tableCheck", time);
     }
     login() {
-        return axios.get("http://www.graduate11.com:8080/api/login2");
+        return axios.get("https://www.graduate11.com:8080/api/login2");
     }
     courseUser(user_no) {
-        return axios.post("http://www.graduate11.com:8080/api/courseUser",user_no);
+        return axios.post("https://www.graduate11.com:8080/api/courseUser",user_no);
     }
     SendClassUser(user_course) {
-        return axios.post("http://www.graduate11.com:8080/api/courseCheck", user_course);
+        return axios.post("https://www.graduate11.com:8080/api/courseCheck", user_course);
     }
     course2User() {
-        return axios.get("http://www.graduate11.com:8080/api/course");
+        return axios.get("https://www.graduate11.com:8080/api/course");
     }
     classUser(user_no) {
-        return axios.post("http://www.graduate11.com:8080/api/classUser", user_no);
+        return axios.post("https://www.graduate11.com:8080/api/classUser", user_no);
     }
     class2User() {
-        return axios.get("http://www.graduate11.com:8080/api/class");
+        return axios.get("https://www.graduate11.com:8080/api/class");
     }
     CheckClassUser(user_class) {
-        return axios.post("http://www.graduate11.com:8080/api/classCheck", user_class);
+        return axios.post("https://www.graduate11.com:8080/api/classCheck", user_class);
     }
     postUser(user_no){
         return axios({method :'post',
-        url:'http://www.graduate11.com:8080/api/courseUser', 
+        url:'https://www.graduate11.com:8080/api/courseUser', 
         headers :{'Content-Type': 'application/json' }, 
         data: user_no});
     }
     SendPre(pre) {
-        return axios.post("http://www.graduate11.com:8080/api/preferenceUser", pre);
+        return axios.post("https://www.graduate11.com:8080/api/preferenceUser", pre);
     }
 }
 
